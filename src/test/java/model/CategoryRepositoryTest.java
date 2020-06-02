7629cd34ffdf8288eb08cd4bb27b7e110636075e
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-02 22:12:17 +0100

    commit to see if working. Created 1 test in CategoryReposioryTest

--- src/test/java/model/CategoryRepositoryTest.java
+++ src/test/java/model/CategoryRepositoryTest.java
@@ -1,7 +1,28 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryRepositoryTest {
+
+    @Test
+    @DisplayName("Verify CategoryRepository constructor && equals")
+    public void accountRepConstructorEquals() {
+
+        // Arrange
+        //Create an arraylist object so that object CategoryRepository can be created
+        ArrayList list1 = new ArrayList();
+        // Act
+        //create 2 CategoryRepositories that can be compared
+        CategoryRepository categoryRepository1 = new CategoryRepository(list1);
+        CategoryRepository categoryRepository2 = new CategoryRepository(list1);
+        // Assert
+        //Verify if constructor works fine and if object is the same
+        assertEquals(categoryRepository1, categoryRepository2);
+    }
 
 }
