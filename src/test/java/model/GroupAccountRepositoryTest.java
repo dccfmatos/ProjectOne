97586aabf31e93b83ce432563a01c5ef87d7f567
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-03 22:53:36 +0100

    Ref #1 - GrouAccounRepositoryTest done

--- src/test/java/model/GroupAccountRepositoryTest.java
+++ src/test/java/model/GroupAccountRepositoryTest.java
@@ -1,7 +1,114 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupAccountRepositoryTest {
+
+    @Test
+    @DisplayName("Verify GroupAccountRepository constructor && equals")
+    public void groupAccountRepositoryConstructorEquals() {
+
+        // Arrange
+        //Create new group account to be used to create group account repositories
+        GroupAccount groupAccount = new GroupAccount("groupAccount Denomination", "groupAccount description");
+        int id = 123;
+
+        // Act
+        //create 2 objects groupAccountRepository
+        GroupAccountRepository groupAccountRepository = new GroupAccountRepository(groupAccount, 123);
+        GroupAccountRepository groupAccountRepository2 = new GroupAccountRepository(groupAccount, id);
+
+        // Assert
+        //Verify if constructor works fine and if object is the same
+        assertEquals(groupAccountRepository, groupAccountRepository2);
+    }
+
+    @Test
+    @DisplayName("Verify getGroupAccountId() of groupAccountRepository")
+    public void accountGetAccountId() {
+
+        // Arrange
+        GroupAccount groupAccount = new GroupAccount("groupAccount Denomination", "groupAccount description");
+        int id = 123;
+
+        // Act
+        //create object groupAccountRepository
+        GroupAccountRepository groupAccountRepository = new GroupAccountRepository(groupAccount, 123);
+
+        // Assert
+        //Verify getter method
+        assertEquals(id, groupAccountRepository.getGroupAccountId());
+    }
+
+    @Test
+    @DisplayName("Verify getGroupAccount() of groupAccountRepository")
+    public void accountGetGroupAccount()  {
+
+        // Arrange
+        GroupAccount groupAccount = new GroupAccount("groupAccount Denomination", "groupAccount description");
+        int id = 123;
+
+        // Act
+        //create object groupAccountRepository
+        GroupAccountRepository groupAccountRepository = new GroupAccountRepository(groupAccount, id);
+
+        // Assert
+        //Verify getter method
+        assertEquals(groupAccount, groupAccountRepository.getGroupAccount());
+    }
+
+    @Test
+    @DisplayName("Verify setGroupAccount() of groupAccountRepository")
+    public void accountSetGroupAccount() {
+
+        // Arrange
+        //create objects groupAccount
+        GroupAccount groupAccount = new GroupAccount("groupAccount Denomination", "groupAccount description");
+        int id = 123;
+
+        GroupAccount groupAccount2 = new GroupAccount("groupAccount Denomination2", "groupAccount description2");
+        int id2 = 1234;
+
+        // Act
+        //create object groupAccountRepository
+        GroupAccountRepository groupAccountRepository = new GroupAccountRepository(groupAccount, id);
+
+        //change parameters for groupAccountRepository using method set()
+        groupAccountRepository.setGroupAccount(groupAccount2);
+        groupAccountRepository.setGroupAccountId(id2);
+
+        // Assert
+        //verify setter method
+        assertEquals(groupAccount2, groupAccountRepository.getGroupAccount());
+    }
+
+    @Test
+    @DisplayName("Verify setGroupAccountId() of groupAccountRepository")
+    public void accountSetGroupAccountId() {
+
+        // Arrange
+        //create objects groupAccount
+        GroupAccount groupAccount = new GroupAccount("groupAccount Denomination", "groupAccount description");
+        int id = 123;
+
+        GroupAccount groupAccount2 = new GroupAccount("groupAccount Denomination2", "groupAccount description2");
+        int id2 = 1234;
+
+        // Act
+        //create object groupAccountRepository
+        GroupAccountRepository groupAccountRepository = new GroupAccountRepository(groupAccount, id);
+
+        //change parameters for groupAccountRepository using method set()
+        groupAccountRepository.setGroupAccount(groupAccount2);
+        groupAccountRepository.setGroupAccountId(id2);
+
+        // Assert
+        //verify setter method
+        assertEquals(id2, groupAccountRepository.getGroupAccountId());
+    }
+
 
 }
--- src/test/java/model/GroupAccountTest.java
+++ src/test/java/model/GroupAccountTest.java
@@ -1,7 +1,120 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupAccountTest {
+
+    @Test
+    @DisplayName("Verify AccountGroup constructor | assertEquals")
+    public void accountGroupConstructor() {
+
+        // Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+
+        // Act
+        GroupAccount accountGroup = new GroupAccount(accountGroupDenomination, accountGroupDescription);
+        GroupAccount accountGroup2 = new GroupAccount("accountGroupDenomination", "accountGroupDescription");
+
+        // Assert
+        assertEquals(accountGroup, accountGroup2);
+    }
+
+    @Test
+    @DisplayName("Verify AccountGroup constructor + group | assertEquals")
+    public void accountGroupConstructorWGroup() {
+
+        // Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+        LocalDate dateOfCreation = LocalDate.of(2020, 01, 26);
+        String denomination = "Group 1";
+
+        Group group1 = new Group(dateOfCreation, denomination);
+
+        // Act
+        GroupAccount accountGroup = new GroupAccount(accountGroupDenomination, accountGroupDescription, group1);
+        GroupAccount accountGroup2 = new GroupAccount("accountGroupDenomination", "accountGroupDescription", group1);
+
+        // Assert
+        assertEquals(accountGroup, accountGroup2);
+    }
+
+
+    @Test
+    @DisplayName("Verify AccountGroup getter | assertEquals")
+    public void accountGroupGetter() {
+
+        //Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+
+        //Act
+        GroupAccount account = new GroupAccount("accountGroupDenomination", "accountGroupDescription");
+
+        //Assert
+        assertEquals(accountGroupDenomination, account.getAccountGroupDenomination());
+    }
+
+    @Test
+    @DisplayName("Verify AccountGroup getter2 | assertEquals")
+    public void accountGroupGetter2() {
+
+        //Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+
+        //Act
+        GroupAccount account = new GroupAccount("accountGroupDenomination", "accountGroupDescription");
+
+        //Assert
+        assertEquals(accountGroupDescription, account.getAccountGroupDescription());
+    }
+
+    @Test
+    @DisplayName("Verify GroupAccount setter | assertEquals")
+    public void accountGroupSetter() {
+
+        //Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+
+        String accountGroupDenomination2 = "accountGroupDenomination2";
+        String accountGroupDescription2 = "accountGroupDescription2";
+
+        //Act
+        GroupAccount account = new GroupAccount(accountGroupDenomination, accountGroupDescription);
+        account.setAccountGroupDenomination(accountGroupDenomination2);
+        account.setAccountGroupDescription(accountGroupDescription2);
+
+        //Assert
+        assertEquals(accountGroupDenomination2, "accountGroupDenomination2");
+    }
+
+    @Test
+    @DisplayName("Verify GroupAccount setter2 | assertEquals")
+    public void accountGroupSetter2() {
+
+        //Arrange
+        String accountGroupDenomination = "accountGroupDenomination";
+        String accountGroupDescription = "accountGroupDescription";
+
+        String accountGroupDenomination2 = "accountGroupDenomination2";
+        String accountGroupDescription2 = "accountGroupDescription2";
+
+        //Act
+        GroupAccount account = new GroupAccount(accountGroupDenomination, accountGroupDescription);
+        account.setAccountGroupDescription(accountGroupDescription2);
+        account.setAccountGroupDenomination(accountGroupDenomination2);
+
+        //Assert
+        assertEquals(accountGroupDescription2, "accountGroupDescription2");
+    }
+
 
 }
