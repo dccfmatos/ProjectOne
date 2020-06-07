88375688ac04e46ba9bf3009e14eebe950474504
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-07 23:08:47 +0100

    Ref #1 - Ledger done

--- src/main/java/model/Ledger.java
+++ src/main/java/model/Ledger.java
@@ -1,8 +1,37 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Ledger {
 
     private ArrayList<Transaction> records;
+
+    public Ledger(ArrayList<Transaction> records) {
+        this.records = records;
+    }
+
+    public ArrayList<Transaction> getRecords() {
+        return records;
+    }
+
+    public void setRecords(ArrayList<Transaction> records) {
+        this.records = records;
+    }
+
+    public void addTransaction(Transaction transaction){
+        if(transaction != null) {
+            this.records.add(transaction);
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ledger)) return false;
+        Ledger ledger = (Ledger) o;
+        return Objects.equals(records, ledger.records);
+    }
+
 }
