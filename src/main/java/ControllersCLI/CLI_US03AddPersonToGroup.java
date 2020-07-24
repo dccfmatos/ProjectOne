e61785ab3754114ac66581dec4c6079bf4c553b9
dccfmatos <d.catarina.c.f@gmail.com>
2020-07-24 17:32:22 +0100

    #5 - Controller US3 + Test

--- src/main/java/ControllersCLI/CLI_US03AddPersonToGroup.java
+++ src/main/java/ControllersCLI/CLI_US03AddPersonToGroup.java
@@ -1,9 +1,14 @@
 package ControllersCLI;
 
+import model.Group;
+import model.Person;
+
+import java.time.LocalDate;
+
 public class CLI_US03AddPersonToGroup {
 
-    public boolean controllersCLI_US03AddPersonToGroup(){
-
-        return false;
+    public Group controllersCLI_US03AddPersonToGroup(Person personToAdd, Group groupAddInto) {
+        groupAddInto.addPersonToGroup(personToAdd);
+        return groupAddInto;
     }
 }
